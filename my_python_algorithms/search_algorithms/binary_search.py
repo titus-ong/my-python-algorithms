e591e0e5f6db8bcbca3e578e3f0ac27de7a51595
Titus Ong <titusongyl@gmail.com>
2020-07-15 16:00:25 +0800

    Update description to include time/space complexities

--- my_python_algorithms/search_algorithms/binary_search.py
+++ my_python_algorithms/search_algorithms/binary_search.py
@@ -1,3 +1,12 @@
+"""
+Binary search algorithm.
+
+Time complexity: O(log(n)) - search space is halved after each iteration
+
+Space complexity: O(1)
+"""
+
+
 def binary_search(array, value) -> int:
     """Return index of the value in the array, or -1 if value is not found."""
     left = 0
