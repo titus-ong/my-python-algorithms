a8b4755f1a39c6a27d9e539af3857687ce1bccf2
Titus Ong <titusongyl@gmail.com>
2020-07-16 14:42:35 +0800

    Include recursive algorithm

--- my_python_algorithms/search_algorithms/binary_search.py
+++ my_python_algorithms/search_algorithms/binary_search.py
@@ -1,5 +1,6 @@
 """
 Binary search algorithm.
+Return index of the value in the array, or -1 if value is not found.
 
 Time complexity: O(log(n)) - search space is halved after each iteration
 
@@ -7,8 +8,8 @@ Space complexity: O(1)
 """
 
 
-def binary_search(array, value) -> int:
-    """Return index of the value in the array, or -1 if value is not found."""
+def binary_search_iterative(array, value) -> int:
+    """Use moving pointers to size the array at each iteration."""
     left = 0
     right = len(array)
     while left < right:
@@ -20,3 +21,20 @@ def binary_search(array, value) -> int:
         if array[mid] < value:
             left = mid+1
     return -1
+
+
+def binary_search_recursive(array, value) -> int:
+    """Recursively search the half of the array that may contain the value."""
+    def recursive(array, left, right, value):
+        if not array[left:right]:
+            return -1
+        mid = (right - left)//2 + left  # take lower mid
+        if array[mid] == value:
+            return mid
+        if array[mid] > value:
+            return recursive(array, left, mid, value)
+        if array[mid] < value:
+            return recursive(array, mid+1, right, value)
+
+    right = len(array)
+    return recursive(array, 0, right, value)
--- tests/test_search_algorithms/test_binary_search.py
+++ tests/test_search_algorithms/test_binary_search.py
@@ -1,38 +1,74 @@
 import pytest
-from my_python_algorithms.search_algorithms.binary_search import binary_search
+from my_python_algorithms.search_algorithms.binary_search import *
 
 
 def test_empty_list():
-    assert -1 == binary_search([], 1)
+    assert -1 == binary_search_iterative([], 1)
 
 
 def test_single_value_not_in_list():
-    assert -1 == binary_search([0], 1)
+    assert -1 == binary_search_iterative([0], 1)
 
 
 def test_value_lower_than_list():
-    assert -1 == binary_search([1, 2, 3, 4, 5], -1)
+    assert -1 == binary_search_iterative([1, 2, 3, 4, 5], -1)
 
 
 def test_intermediate_value():
-    assert -1 == binary_search([1, 2, 3], 2.5)
+    assert -1 == binary_search_iterative([1, 2, 3], 2.5)
 
 
 def test_value_higher_than_list():
-    assert -1 == binary_search([1, 2, 3], 5)
+    assert -1 == binary_search_iterative([1, 2, 3], 5)
 
 
 def test_single_value_in_list():
-    assert 0 == binary_search([1], 1)
+    assert 0 == binary_search_iterative([1], 1)
 
 
 def test_value_in_list_1():
-    assert 2 == binary_search([-5, -3, 0, 1], 0)
+    assert 2 == binary_search_iterative([-5, -3, 0, 1], 0)
 
 
 def test_value_in_list_2():
-    assert 0 == binary_search([-100, 1, 2, 3], -100)
+    assert 0 == binary_search_iterative([-100, 1, 2, 3], -100)
 
 
 def test_value_in_list_3():
-    assert 5 == binary_search([-5, 1, 2, 4, 6, 10], 10)
+    assert 5 == binary_search_iterative([-5, 1, 2, 4, 6, 10], 10)
+
+
+def test_empty_list_rec():
+    assert -1 == binary_search_recursive([], 1)
+
+
+def test_single_value_not_in_list_rec():
+    assert -1 == binary_search_recursive([0], 1)
+
+
+def test_value_lower_than_list_rec():
+    assert -1 == binary_search_recursive([1, 2, 3, 4, 5], -1)
+
+
+def test_intermediate_value_rec():
+    assert -1 == binary_search_recursive([1, 2, 3], 2.5)
+
+
+def test_value_higher_than_list_rec():
+    assert -1 == binary_search_recursive([1, 2, 3], 5)
+
+
+def test_single_value_in_list_rec():
+    assert 0 == binary_search_recursive([1], 1)
+
+
+def test_value_in_list_1_rec():
+    assert 2 == binary_search_recursive([-5, -3, 0, 1], 0)
+
+
+def test_value_in_list_2_rec():
+    assert 0 == binary_search_recursive([-100, 1, 2, 3], -100)
+
+
+def test_value_in_list_3_rec():
+    assert 5 == binary_search_recursive([-5, 1, 2, 4, 6, 10], 10)
